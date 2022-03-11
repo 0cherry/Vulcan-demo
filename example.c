6bb09b26be809ffe7ded33d8637d0fcde8d2b34c
cherryflavor <yckimcherry@gmail.com>
2022-03-11 02:24:52 +0000

    Revised to test issue

--- example.c
+++ example.c
@@ -1,6 +1,21 @@
 int func1(int x) {
     int y = 100;
     y = x;
+    if (x == 10) {
+        return -y;
+    }
+    if (x == 20) {
+        return -y;
+    }
+    if (x == 30) {
+        return -y;
+    }
+    if (x == 40) {
+        return -y;
+    }
+    if (x == 50) {
+        return -y;
+    }
     if (x >= 100) { // correct code: if (x > 100) {
         return -y;
     }
